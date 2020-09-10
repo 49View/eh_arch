234783283129a9008d092b956d23f9f9cf97706b
Shinobi <alberto.prina@gmail.com>
2020-09-10 12:10:24 +0200

    Solved problem for road closed path

--- tools/osm/osm2polygons/roads.js
+++ tools/osm/osm2polygons/roads.js
@@ -110,6 +110,7 @@ const roadFromWay = (way) => {
                 roadLane = 1;
                 roadColor = "#FFFFFF";
         }
+        let faces=[]
         let points=[];
         way.nodes.forEach(n => {
             const point = { ...n };
@@ -118,31 +119,59 @@ const roadFromWay = (way) => {
             points.push(point);
         }) ;
 
-        let endType = ClipperLib.EndType.etOpenRound;
-        if (points[0].id===points[points.length-1].id) {
-            //endType = ClipperLib.EndType.etClosedLine;
-            points.splice(points.length-1,1);
-        }
-
         const localBoundingBox = computeBoundingBox(points);
         convertToLocalCoordinate(points, localBoundingBox.centerX, localBoundingBox.centerY);
 
-        var subj = new ClipperLib.Paths();
-        var solution = new ClipperLib.Paths();
-        subj[0] = points.map(p => { return {X: p.x, Y: p.y}});
-        var scale = 100;
-        ClipperLib.JS.ScaleUpPaths(subj, scale);
-        var co = new ClipperLib.ClipperOffset(2, 0.25);
-        co.AddPaths(subj, ClipperLib.JoinType.jtRound, endType);
-        co.Execute(solution, roadWidth*roadLane*scale);
-        ClipperLib.JS.ScaleDownPaths(solution, scale);
 
-        let faces=[]
-        solution.forEach(s => {
-            const polygon = s.map(p => { return { x: p.X, y:p.Y}});
-            faces = faces.concat(getTrianglesFromPolygon(polygon));
+        let pointsPartial = [];
+
+        // if (points[0].id===points[points.length-1].id) {
+        //     //endType = ClipperLib.EndType.etClosedLine;
+        //     //points.splice(points.length-1,1);
+        //     pointsPartial.push(points.slice(0, points.length-1));
+        //     pointsPartial.push(points.slice(points.length-2, points.length));
+        // } else {
+        //     pointsPartial.push(points);
+        // }
+
+        let startIndex = 0;
+        for (let i=startIndex;i<points.length;i++) {
+            for (j=i+1;j<points.length;j++) {
+                if (points[i].id===points[j].id) {
+                    if (startIndex<j) {
+                        pointsPartial.push(points.slice(startIndex, j));
+                    }
+                    startIndex=j;
+                    i=j;
+                    break;
+                }
+            }            
+        }
+
+        if (startIndex===0) {
+            pointsPartial.push(points);
+        } else {
+            pointsPartial.push(points.slice(startIndex-1, points.length));
+        }
+
+        pointsPartial.forEach(points => {
+            let subj = new ClipperLib.Paths();
+            let solution = new ClipperLib.Paths();
+            subj[0] = points.map(p => { return {X: p.x, Y: p.y}});
+            var scale = 100;
+            ClipperLib.JS.ScaleUpPaths(subj, scale);
+            var co = new ClipperLib.ClipperOffset(2, 0.25);
+            co.AddPaths(subj, ClipperLib.JoinType.jtRound, ClipperLib.EndType.etOpenRound);
+            co.Execute(solution, roadWidth*roadLane*scale);
+            ClipperLib.JS.ScaleDownPaths(solution, scale);
+    
+            solution.forEach(s => {
+                const polygon = s.map(p => { return { x: p.X, y:p.Y}});
+                faces = faces.concat(getTrianglesFromPolygon(polygon));
+            });
         });
-        setHeight(faces,3);
+
+        setHeight(faces,1.5);
 
         road = createRoadMesh("w-"+way.id, way.tags, localBoundingBox, faces, "#000000");
     } catch (ex) {
