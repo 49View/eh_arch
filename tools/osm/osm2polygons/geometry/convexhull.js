7b439813b15bf6c9a7b1a755fbebd79da15cbb64
DadoLinux <ziocleto@gmail.com>
2020-10-08 09:11:39 +0100

    convexhull now uses modern ES6 syntax

--- tools/osm/osm2polygons/geometry/convexhull.js
+++ tools/osm/osm2polygons/geometry/convexhull.js
@@ -2,22 +2,23 @@
 
 const {Vector} = require("./vector");
 
-var ON = 0;
-var LEFT = 1;
-var RIGHT = 2;
-var ALMOST_ZERO = 0.0;
+const ON = 0;
+const LEFT = 1;
+const RIGHT = 2;
+const ALMOST_ZERO = 0.0;
 
 function GetSideOfLine(lineStart, lineEnd, point)
 {
-    var d = (lineEnd.x-lineStart.x)*(point.y-lineStart.y)-(lineEnd.y-lineStart.y)*(point.x-lineStart.x);
-    return (d > ALMOST_ZERO ? LEFT : (d < -ALMOST_ZERO ? RIGHT : ON));
+  const d = (lineEnd.x - lineStart.x) * (point.y - lineStart.y) - (lineEnd.y - lineStart.y) * (point.x - lineStart.x);
+  return (d > ALMOST_ZERO ? LEFT : (d < -ALMOST_ZERO ? RIGHT : ON));
 }
 
 // returns convex hull in CW order
 // (required by Rotating Calipers implementation)
 function calcConvexHull(points)
 {
-    const vectors=[];
+    let endPt;
+  const vectors=[];
 
     points.forEach(p => {
       vectors.push(new Vector(p.x, p.y));
@@ -35,10 +36,10 @@ function calcConvexHull(points)
     }
 
     // find first hull point
-    var hullPt = vectors[0];
-    var convexHull = [];
+    let hullPt = vectors[0];
+    let convexHull = [];
 
-    for (var i=1; i<vectors.length; i++)
+    for (let i=1; i<vectors.length; i++)
     {
         // perform lexicographical compare
         if (vectors[i].x < hullPt.x)
@@ -52,15 +53,15 @@ function calcConvexHull(points)
     do
     {
         convexHull.unshift(hullPt.clone());
-        var endPt = vectors[0];
+        endPt = vectors[0];
 
-        for (var j=1; j<vectors.length; j++)
+        for (let j=1; j<vectors.length; j++)
         {
-            var side = GetSideOfLine(hullPt, endPt, vectors[j]);
+          const side = GetSideOfLine(hullPt, endPt, vectors[j]);
 
-            // in case point lies on line take the one further away.
-            // this fixes the collinearity problem.
-            if (endPt.equals(hullPt) || (side == LEFT || (side == ON && hullPt.distance(vectors[j]) > hullPt.distance(endPt))))
+          // in case point lies on line take the one further away.
+            // this fixes the co-linearity problem.
+            if (endPt.equals(hullPt) || (side === LEFT || (side === ON && hullPt.distance(vectors[j]) > hullPt.distance(endPt))))
                 endPt = vectors[j];
         }
 
